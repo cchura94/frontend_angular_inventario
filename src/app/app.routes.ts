20e448a006d4af349cf152efec695b03a4ece5d2
Cristian <cchura.cpc@gmail.com>
2025-12-02 21:50:06 -0400

    Estructura de proyecto web - auth - admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './web/inicio/inicio';
+import { Nosotros } from './web/nosotros/nosotros';
+import { Galeria } from './web/galeria/galeria';
+import { Profile } from './admin/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Inicio },
+    { path: 'nosotros', component: Nosotros },
+    { path: 'galeria', component: Galeria },
+
+    { path: 'auth', loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule) },
+
+    { path: 'admin/perfil', component: Profile }
+
+];
