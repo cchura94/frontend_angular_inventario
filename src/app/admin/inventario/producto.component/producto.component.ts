1ba415e295db1c2a19dd5a6bb8a3772d50b4b11b
Cristian <cchura.cpc@gmail.com>
2025-12-15 22:30:46 -0400

    productos

--- src/app/admin/inventario/producto.component/producto.component.ts
+++ src/app/admin/inventario/producto.component/producto.component.ts
@@ -1,29 +1,153 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
-import { Table } from 'primeng/table';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { Table, TableModule } from 'primeng/table';
+import /*ConfirmationService, MessageService*/ 'primeng/api';
+import { Dialog } from 'primeng/dialog';
+import { Ripple } from 'primeng/ripple';
+import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RadioButton } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import { InputNumber } from 'primeng/inputnumber';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { TextareaModule } from 'primeng/textarea';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { ProductoService } from '../../../core/services/producto.service';
+
+interface Column {
+  field: string;
+  header: string;
+  customExportHeader?: string;
+}
+
+interface ExportColumn {
+  title: string;
+  dataKey: string;
+}
 
 @Component({
   selector: 'app-producto.component',
-  imports: [ToolbarModule, ButtonModule],
+  imports: [
+    TableModule,
+    Dialog,
+    Ripple,
+    SelectModule,
+    ToastModule,
+    ToolbarModule,
+    ConfirmDialog,
+    InputTextModule,
+    TextareaModule,
+    CommonModule,
+    RadioButton,
+    InputTextModule,
+    InputNumber,
+    IconFieldModule,
+    InputIconModule,
+    ButtonModule,
+    FormsModule
+  ],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss',
 })
-export class ProductoComponent implements OnInit {
-
+export class ProductoComponent {
   // @ViewChild('dt') dt!: Table;
 
-  
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  openNew(){
+  products = signal([]);
+  product = signal<any>({});
+  cols!: Column[];
+  submitted= signal<boolean>(false);
+  productDialog = signal<boolean>(false);
+  @ViewChild('dt') dt!: Table;
 
+  productoService = inject(ProductoService);
+
+  constructor(
+    // private messageService: MessageService,
+    // private confirmationService: ConfirmationService,
+    private cd: ChangeDetectorRef
+  ) {
+
+    this.funGetProductos();
   }
+
+  funGetProductos(){
+    this.productoService.funListar().subscribe(
+      (res: any) => {
+        console.log(res)
+        this.products.set(res.data)
+      }
+    )
+  }
+
+  hideDialog() {
+      this.productDialog.set(false);
+      this.submitted.set(false);
+  }
+
+  openNew() {
+    this.product.set({});
+    this.submitted.set(false);
+    this.productDialog.set(true);
+}
 
   exportCSV(event: any) {
     // this.dt.exportCSV();
   }
+
+  editProduct(product: any) {
+    this.product = { ...product };
+    this.productDialog.set(true);
+  }
+
+  deleteProduct(product: any) {
+    /*
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + product.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: 'No',
+        severity: 'secondary',
+        variant: 'text',
+      },
+      acceptButtonProps: {
+        severity: 'danger',
+        label: 'Yes',
+      },
+      accept: () => {
+        this.products.set(this.products().filter((val: any) => val.id !== product.id));
+        this.product.set({});
+        
+          this.messageService.add({
+              severity: 'success',
+              summary: 'Successful',
+              detail: 'Producto Eliminado',
+              life: 3000
+          });
+          
+      },
+    });
+    */
+  }
+
+  saveProduct() {
+    this.submitted.set(true);
+
+    if (this.product.name?.trim()) {
+        if (this.product().id) {
+            
+        } else {
+           
+        }
+
+        this.products.set([...this.products()]);
+        this.productDialog.set(false);
+        this.product.set({});
+    }
+}
+
 }
---
+++ src/app/core/services/producto.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductoService {
+  urlBase = environment.servidor;
+  http = inject(HttpClient);
+  
+  funListar(page: number = 1, limit: number = 10, search: string = ''){
+    return this.http.get(`${this.urlBase}/producto`);
+  }
+
+  funGuardar(datos: any){
+    return this.http.post(`${this.urlBase}/producto`, datos);
+  }
+
+  funMostrar(id: number){
+    return this.http.get(`${this.urlBase}/producto/${id}`);
+  }
+
+  funModificar(id: number, datos: any){
+    return this.http.patch(`${this.urlBase}/producto/${id}`, datos);
+  }
+
+  funEliminar(id: number){
+    return this.http.delete(`${this.urlBase}/producto/${id}`);
+  }
+
+}
